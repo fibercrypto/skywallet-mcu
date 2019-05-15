08117ef12761b133571595e80eaddff9cd245f98
Alvaro Denis <dev0003@simelo.tech>
2019-05-14 20:00:28 -0400

    add SKY_cipher_Address_Verify ref #84

--- skycoin-api/libskycoin-wrapper.c
+++ skycoin-api/libskycoin-wrapper.c
@@ -1,10 +1,16 @@
 #include "libskycoin.h"
 
 #include <stdint.h>
+#include <string.h>
+
+#include "skyerrors.h"
+
+#include "sha2.h"
+#include "ripemd160.h"
 
 GoUint32 SKY_cipher_SumSHA256(GoSlice p0, cipher__SHA256* p1) {
     compute_sha256sum(p0.data, p1, sizeof(p1));
-    return 0;
+    return SKY_OK;
 }
 
 GoUint32 SKY_cipher_SHA256FromHex(GoString p0, cipher__SHA256* p1) {
@@ -27,16 +33,28 @@ GoUint32 SKY_cipher_GenerateKeyPair(cipher__PubKey* p0, cipher__SecKey* p1) {
     memcpy(&seedd, &seed, sizeof(seed));
     compute_sha256sum((const uint8_t*)seedd, digest, sizeof(seed));
     generate_deterministic_key_pair(digest, sizeof(digest), p1, p0);
-    return 0;
+    return SKY_OK;
 }
 
 GoUint32 SKY_cipher_GenerateDeterministicKeyPair(GoSlice p0, cipher__PubKey* p1, cipher__SecKey* p2) {
     generate_deterministic_key_pair(p0.data, p0.len, p1, p2);
-    return 0;
+    return SKY_OK;
 }
 
 GoUint32 SKY_cipher_AddressFromPubKey(cipher__PubKey* p0, cipher__Address* p1) {
     size_t size_address = 0;;
     generate_base58_address_from_pubkey(p0, p1->Key, &size_address);
-    return 0;
+    return SKY_OK;
+}
+
+GoUint32 SKY_cipher_Address_Verify(cipher__Address* p0, cipher__PubKey* p1) {
+    if (p0->Version != 0x00) {
+        return SKY_ErrAddressInvalidVersion;
+    }
+    uint8_t hash[RIPEMD160_DIGEST_LENGTH] = {0};
+    ripemd160((int*)p1, sizeof(cipher__PubKey), hash);
+    if (strncmp(p0->Key, hash, sizeof(p0->Key))) {
+        return SKY_ErrAddressInvalidPubKey;
+    }
+    return SKY_OK;
 }
