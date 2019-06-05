c0921fdfdd963571152485e7a9a8e2de6ec6ecf1
Alvaro Denis <dev0003@simelo.tech>
2019-06-05 18:00:42 -0400

    use pub_key_ripemd160 in SKY_cipher_Address_Verify instead of ripemd160 ref #84

--- skycoin-api/libskycoin-wrapper.c
+++ skycoin-api/libskycoin-wrapper.c
@@ -65,7 +65,7 @@ GoUint32 SKY_cipher_Address_Verify(cipher__Address* p0, cipher__PubKey* p1) {
         return SKY_ErrAddressInvalidVersion;
     }
     uint8_t hash[RIPEMD160_DIGEST_LENGTH] = {0};
-    ripemd160((const uint8_t*)p1, sizeof(cipher__PubKey), hash);
+    pub_key_ripemd160((const uint8_t*)p1, sizeof(cipher__PubKey), hash);
     if (memcmp((const char*)(p0->Key), (const char*)hash, sizeof(p0->Key))) {
         return SKY_ErrAddressInvalidPubKey;
     }
