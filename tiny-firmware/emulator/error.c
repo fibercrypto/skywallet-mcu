6e82f51d8cb6194776b3ea40f0c49f1a8a0dcd75
Alvaro Denis <dev0003@simelo.tech>
2019-07-12 15:59:03 -0400

    build emulator/error.c in testing mode to avoid putting the graphyc stack in crypto tests ref #226

--- tiny-firmware/emulator/error.c
+++ tiny-firmware/emulator/error.c
@@ -11,13 +11,24 @@
 
 #include <unistd.h>
 
+#ifdef TESTING
+#include <stdio.h>
+#else
 #include "layout.h"
 #include "oled.h"
+#endif //  TESTING
+
 void __attribute__((noreturn)) panic(char *msg) {
-  layoutDialog(&bmp_icon_error, NULL, NULL, NULL, NULL, msg, "detected.",
-               "Please unplug", "the device.", NULL);
+#ifdef TESTING
+    printf("msg: %s\n", msg);
+#else
+    layoutDialog(&bmp_icon_error, NULL, NULL, NULL, NULL, msg, "detected.",
+                 "Please unplug", "the device.", NULL);
+#endif //  TESTING
   while (1) {
+#if !defined(TESTING)
     oledRefresh();
+#endif //  !deffined(TESTING)
     sleep(1);
   }  // loop forever
 }
